43a40a0e2bcb4064c203241a7448cfb869a4a1a3
kveloria3 <klveloria@yahoo.com>
2014-12-19 22:01:41 -0700

    Update CellString.java

--- src/CellString.java
+++ src/CellString.java
@@ -1,1 +1,29 @@
 // placeholder
+public class CellString extends Cell
+{
+  public CellString()
+  {
+    super();
+  }
+  public CellString(String val)
+  {
+    setValue(val);
+  }
+  public String toString()
+  {
+    String result = getValue();
+    if(result.length() == 0)
+    {
+      result = "<empty>";
+    }
+    else
+    {
+      result = "\"" + result + "\"";
+    }
+    return result;
+  }
+  public String setValue()
+  {
+    return setValue();
+  }
+}
