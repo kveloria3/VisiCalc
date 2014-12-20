98ce43dd89acd9a6888271cdb552e5ba121d2b1e
kveloria3 <klveloria@yahoo.com>
2014-12-19 22:06:37 -0700

    Update VisiCalc.java

--- src/VisiCalc.java
+++ src/VisiCalc.java
@@ -1,17 +1,23 @@
 public class VisiCalc {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// create an array of cells
-
+		Cell cells = new Cell[];
+		
 		
 		// initialize the array
 
 
 		
 		// add test values
-
+		//I don't now what these are!!!
 		
 		// print the array
-
+		for(int i=0; i<cells.length-1; i++)
+		{
+			System.out.print(cells[i]+", ")
+		}
+		System.out.print(cells[cells.legnth]);
 	}
 }
