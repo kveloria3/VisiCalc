0ca9a4cf5752fb6e9cf091a4d6fb845da142d01c
kveloria3 <klveloria@yahoo.com>
2014-12-19 22:02:01 -0700

    Update CellNumeric.java

--- src/CellNumeric.java
+++ src/CellNumeric.java
@@ -1,1 +1,20 @@
 // placeholder
+public class CellNumeric extends Cell
+{
+  public CellNumeric()
+  {
+    super();
+  }
+  public CellNumeric(String d)
+  {
+    setValue(d);
+  }
+  public String value()
+  {
+    return getValue();
+  }
+  public String setValue(String input)
+  {
+    return setValue(input);
+  }
+}
