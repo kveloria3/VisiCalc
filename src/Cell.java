587cb7f2b0c309e512699832f7a63c57b2e12525
kveloria3 <klveloria@yahoo.com>
2014-12-19 22:03:15 -0700

    Update Cell.java

--- src/Cell.java
+++ src/Cell.java
@@ -1,1 +1,47 @@
 // placeholder
+public class Cell
+{
+  String value;
+  public Cell()
+  {
+    value = " ";
+  }
+  public Cell(String val)
+  {
+    value = val;
+  }
+  public String getValue()
+  {
+    return value;
+  }
+  public String toDisplay()
+  {
+    int columnWidth = 12;
+    String output = value;
+    if(output.length()>columnWidth)
+    {
+      output = output.substring(0, columnWidth - 1) + ">";
+    }
+    if(output.length()%2 == 1)
+    {
+      output += " ";
+    }
+    while(output.length()<columnWidth)
+    {
+      output = " " + output + " ";
+    }
+    return output;
+  }
+  
+  public String toString()
+  {
+    String result = getValue();
+    if(result.length() == 0)
+    {
+      result = "<empty>";
+    }
+    return result;
+  }
+}
+  
+ 
